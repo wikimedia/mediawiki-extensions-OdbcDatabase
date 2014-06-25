307467a996d71e9e086c0b1ef56b0d3e86ebc5a0
Raimond Spekking <raimond.spekking@gmail.com>
2014-06-25 15:27:36 +0200

    Consistency tweaks in preparation for adding to translatewiki.net
    * Rename alias file
    * Sprinkle some <code></code>
    
    Change-Id: I604f1b1023bfe0d44d0540dabd45ef416af68a87

--- OdbcDatabase.php
+++ OdbcDatabase.php
@@ -11,12 +11,12 @@ $wgExtensionCredits['other'][] = array(
 	'author' => 'Roger Cass',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:OdbcDatabase',
 	'descriptionmsg' => 'odbcdatabase-desc',
-	'version' => '1.2.0 alpha 1',
+	'version' => '1.2.0 alpha 2',
 );
 
 $wgMessagesDirs['OdbcDatabase'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['OdbcDatabase'] = __DIR__ . '/OdbcDatabase.i18n.php';
-$wgExtensionMessagesFiles['OdbcDatabaseAlias'] = __DIR__ . '/OdbcDatabase.aliases.php';
+$wgExtensionMessagesFiles['OdbcDatabaseAlias'] = __DIR__ . '/OdbcDatabase.alias.php';
 $wgAutoloadClasses['DatabaseOdbc']
 	= $wgAutoloadClasses['OdbcField']
 	= __DIR__ . '/OdbcDatabase.body.php';
