90b37140ff2dd234f852fffacfa03f7aafb59b31
anterdc99 <xuminruixuan@live.cn>
2025-12-02 20:11:19 +0800

    OdbcDatabase: Update Chinese translations
    What:
    - Set Chinese primary special page alias to the English primary
    - Improve existing Chinese aliases translations
    
    Bug: T399285
    Change-Id: I46794007adcf5103da76ce75ff904b0a4e8b1e8f

--- OdbcDatabase.alias.php
+++ OdbcDatabase.alias.php
@@ -58,12 +58,17 @@ $specialPageAliases['vi'] = [
 	'OdbcDatabase' => [ 'CSDL_ODBC', 'Cơ_sở_dữ_liệu_ODBC' ],
 ];
 
-/** Simplified Chinese (中文（简体）‎) */
-$specialPageAliases['zh-hans'] = [
-	'OdbcDatabase' => [ 'Odbc数据库' ],
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'OdbcDatabase' => [ 'OdbcDatabase' ],
 ];
 
-/** Traditional Chinese (中文（繁體）‎) */
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'OdbcDatabase' => [ 'ODBC数据库' ],
+];
+
+/** Traditional Chinese (中文（繁體）) */
 $specialPageAliases['zh-hant'] = [
-	'OdbcDatabase' => [ 'Odbc_資料庫' ],
+	'OdbcDatabase' => [ 'ODBC資料庫', 'ODBC_資料庫' ],
 ];
